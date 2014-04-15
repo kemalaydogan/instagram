e849444c72d5768f87be4972ae913ab71336aac5
BonoboUK <mark@superius.co.uk>
2014-04-15 16:15:52 +0100

    Added a bit of CSS

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all 
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new params[:post].permit(:description, :picture)
+		if @post.save
+			redirect_to '/posts'
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@post = Post.find params[:id]
+		@post.destroy
+
+		redirect_to '/posts'
+	end
+end
---
+++ spec/features/upload_photos_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'Uploading photos' do 
+	it 'displayed the image on the post page' do
+		visit '/posts/new'
+		fill_in 'Description', with: 'My holiday pic'
+		attach_file 'Picture', Rails.root.join('spec/images/old-man.jpg')
+		click_button 'Create Post'
+
+		expect(page).to have_css 'img.uploaded-pic'
+	end
+end
